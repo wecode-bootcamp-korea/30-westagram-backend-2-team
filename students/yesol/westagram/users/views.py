6d9c1cb49cffaad6fef2c676da4d0b1f1aacbdca
lesol <wingnawing@gmail.com>
2022-02-17 20:27:41 +0900

    Create: users app >> member register view

--- students/yesol/westagram/users/views.py
+++ students/yesol/westagram/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import Member
+
+class MembersView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            existing_email_list = Member.objects.all()
+            email_list = []
+
+            for user_email in existing_email_list:
+                email_list.append(user_email.email)
+
+            if data["email"]=="" or data["password"]=="" :
+                return JsonResponse({"results":"KEY_ERROR"}, status=400)
+            elif re.search("[@]", data["email"])==None or re.search("[.]", data["email"])==None :
+                return JsonResponse({"results":"EMAIL_ERROR"}, status=400)
+            elif len(data["password"])<8 or re.search("[a-zA-Z0-9_]", data["password"])==None or re.search("[^a-zA-Z0-9_]", data["password"])==None:
+                return JsonResponse({"results":"PASSWORD_ERROR"}, status=400)
+            elif data["email"] in email_list :
+                return JsonResponse({"results":"ALREADY_EXISTS"}, status=400)
+            else:
+                Member.objects.create(
+                    name         = data["name"],
+                    email        = data["email"],
+                    password     = data["password"],
+                    phone_number = data["phone_number"],
+                    created_at   = data["created_at"],
+                    updated_at   = data["updated_at"],
+                )
+            return JsonResponse({"results":"SUCCESS"}, status=201)
+        except KeyError:
+            return JsonResponse({"results":"KEY_ERROR"}, status=400)
+
+
