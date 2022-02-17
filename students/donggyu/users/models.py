0e4a30e2443d760a67cc358f51105b29b646afcd
DGK <rlaehdrb0923@gmail.com>
2022-02-17 18:10:52 +0900

    Modify : models.py

--- students/donggyu/users/models.py
+++ students/donggyu/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     first_name      = models.CharField(max_length=50)
     last_name       = models.CharField(max_length=50)
-    email           = models.CharField(max_length=100)
+    email           = models.CharField(max_length=100, unique=True)
     password        = models.CharField(max_length=100)
     phone_number    = models.CharField(max_length=100)
     created_at      = models.DateTimeField(auto_now_add=True)
