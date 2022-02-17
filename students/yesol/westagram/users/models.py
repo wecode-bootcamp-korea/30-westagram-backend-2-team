ea63fe9cbbc52aca6b7fe6bd5d011421c8a02505
lesol <wingnawing@gmail.com>
2022-02-17 16:17:00 +0900

    Edit: models.py >> convention

--- students/yesol/westagram/users/models.py
+++ students/yesol/westagram/users/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
+    name     = models.CharField(max_length=20)
+    email    = models.CharField(max_length=50)
     password = models.CharField(max_length=20)
-    phone = models.CharField(max_length=30)
+    phone    = models.CharField(max_length=30)
 
     class Meta:
         db_table = "members"
