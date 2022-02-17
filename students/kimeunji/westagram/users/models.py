3f4045bb62200ab5c04a27ff1dab520d7d846dc3
kim-eunji <kim-eunji@kim-eunji.local>
2022-02-17 18:42:41 +0900

    modify : modified models.py
    	- add Unique option to EmailField
    	- modified Max_length of password
    `	- phone_number = BigIntegerField to CharField
    	- created 'created_at', 'updated_at'

--- students/kimeunji/westagram/users/models.py
+++ students/kimeunji/westagram/users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class User(models.Model):
     name         = models.CharField(max_length=50)
-    email        = models.CharField(max_length=300)
-    password     = models.CharField(max_length=50)
-    phone_number = models.BigIntegerField()
+    email        = models.EmailField(max_length=254, Unique=True)
+    password     = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=50)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = 'users'
