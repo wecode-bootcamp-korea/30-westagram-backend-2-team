3441614218b1d5d28e5a70dd048d9c13969b7834
kkookkss <pre0919@gmail.com>
2022-02-17 17:46:47 +0900

    westagram project model modified

--- students/myungkookyoon/users/models.py
+++ students/myungkookyoon/users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    user_name    = models.CharField(max_length=10)
-    email        = models.EmailField(max_length=20)
-    password     = models.CharField(max_length=20)
+    username     = models.CharField(max_length=30)
+    email        = models.EmailField(max_length=20, unique=True)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
+    
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
