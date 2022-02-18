b3eec8dbe0f1180e6d54be3a0cc335d5ab3f8136
lesol <wingnawing@gmail.com>
2022-02-18 16:05:18 +0900

    Edit: users.views.py >> keyerror validation, readability

--- students/yesol/westagram/users/views.py
+++ students/yesol/westagram/users/views.py
@@ -1,6 +1,6 @@
 import json, re
 
-from django.http import JsonResponse
+from django.http  import JsonResponse
 from django.views import View
 
 from users.models import Member
@@ -10,29 +10,25 @@ class MembersView(View):
         try:
             data = json.loads(request.body)
 
-            email_list = []
-            existing_email_list = Member.objects.all()
+            # if data["email"]=="" or data["password"]=="" :
+            #     return JsonResponse({"results":"KEY_ERROR"}, status=400)
             
-            for user_email in existing_email_list:
-                email_list.append(user_email.email)
-
-            if data["email"]=="" or data["password"]=="" :
-                return JsonResponse({"results":"KEY_ERROR"}, status=400)
-            elif re.search("[@]", data["email"])==None or re.search("[.]", data["email"])==None :
+            if re.search("[@]", data["email"])==None or re.search("[.]", data["email"])==None :
                 return JsonResponse({"results":"EMAIL_ERROR"}, status=400)
-            elif len(data["password"])<8 or re.search("[a-zA-Z0-9_]", data["password"])==None or re.search("[^a-zA-Z0-9_]", data["password"])==None:
+            
+            if len(data["password"])<8 or re.search("[a-zA-Z0-9_]", data["password"])==None or re.search("[^a-zA-Z0-9_]", data["password"])==None:
                 return JsonResponse({"results":"PASSWORD_ERROR"}, status=400)
-            elif data["email"] in email_list :
+            
+            if Member.objects.filter(email=data["email"]).exists() :
                 return JsonResponse({"results":"ALREADY_EXISTS"}, status=400)
-            else:
-                Member.objects.create(
-                    name         = data["name"],
-                    email        = data["email"],
-                    password     = data["password"],
-                    phone_number = data["phone_number"],
-                    created_at   = data["created_at"],
-                    updated_at   = data["updated_at"],
-                )
+            
+            
+            Member.objects.create(
+                name         = data["name"],
+                email        = data["email"],
+                password     = data["password"],
+                phone_number = data["phone_number"]
+            )
             return JsonResponse({"results":"SUCCESS"}, status=201)
         except KeyError:
             return JsonResponse({"results":"KEY_ERROR"}, status=400)
