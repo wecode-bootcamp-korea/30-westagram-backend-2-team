b4478203d34bb8a5fe24b93173454ecab3fa91bc
DGK <rlaehdrb0923@gmail.com>
2022-02-18 14:10:55 +0900

    Add : views.py

--- students/donggyu/users/views.py
+++ students/donggyu/users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User
+
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            first_name   = data['first_name']
+            last_name    = data['last_name']
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'message' : 'ALREADY_EXISTED_EMAIL'}, status=400)
+            
+            email_regexp    = '^\w+([\.-]?\w+)*@\w+(\.\w{2,3})+$'
+            password_regexp = '\S{8,20}'
+            
+            if not re.match(email_regexp, email):
+                return JsonResponse({'message' : 'INVALID_EMAIL'}, status=400)
+            if not re.match(password_regexp, password):
+                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status=400)
+            
+            User.objects.create(
+                first_name   = first_name,
+                last_name    = last_name,
+                email        = email,
+                password     = password,
+                phone_number = phone_number)
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
