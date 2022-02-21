e63cfe1ad13ab3d3c09e62d07dc8458e17231cfc
lesol <wingnawing@gmail.com>
2022-02-21 15:00:22 +0900

    Edit: MembersRegisterView >> hashing password by bcrypt

--- students/yesol/westagram/users/views.py
+++ students/yesol/westagram/users/views.py
@@ -1,4 +1,4 @@
-import json, re
+import json, re, bcrypt as b
 
 from django.http  import JsonResponse
 from django.views import View
@@ -9,22 +9,26 @@ class MembersRegisterView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
+            email = data["email"]
+            input_password = data["password"]
             
-            if not re.search("[@]", data["email"]) or not re.search("[.]", data["email"]) :
+            if not re.search("[@]", email) or not re.search("[.]", email) :
                 return JsonResponse({"results":"EMAIL_ERROR"}, status=400)
             
-            if len(data["password"])<8 or not re.search("[a-zA-Z0-9_]", data["password"]) or not re.search("[^a-zA-Z0-9_]", data["password"]) :
+            if len(input_password)<8 or not re.search("[a-zA-Z0-9_]", input_password) or not re.search("[^a-zA-Z0-9_]", input_password) :
                 return JsonResponse({"results":"PASSWORD_ERROR"}, status=400)
             
-            if Member.objects.filter(email=data["email"]).exists() :
+            if Member.objects.filter(email=email).exists() :
                 return JsonResponse({"results":"ALREADY_EXISTS"}, status=400)
-            
-            Member.objects.create(
-                name         = data["name"],
-                email        = data["email"],
-                password     = data["password"],
-                phone_number = data["phone_number"]
-            )
+
+            hashed_password = b.hashpw(input_password.encode("utf-8"), b.gensalt()) 
+            if b.checkpw(input_password.encode("utf-8"), hashed_password):
+                Member.objects.create(
+                    name         = data["name"],
+                    email        = email,
+                    password     = hashed_password,
+                    phone_number = data["phone_number"]
+                )
             return JsonResponse({"results":"SUCCESS"}, status=201)
         except KeyError:
             return JsonResponse({"results":"KEY_ERROR"}, status=400)
@@ -36,7 +40,7 @@ class MembersLoginView(View):
 
             if not Member.objects.filter(email=data["email"]).exists() or Member.objects.get(email=data["email"]).password != data["password"] :
                 return JsonResponse({"results":"INVALID_USER"}, status=401)
-
+                
             return JsonResponse({"results":"SUCCESS"}, status=201)
         except KeyError:
             return JsonResponse({"results":"KEY_ERROR"}, status=400)
