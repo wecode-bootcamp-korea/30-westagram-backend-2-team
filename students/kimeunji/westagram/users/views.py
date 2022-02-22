6c9b379428d5359b10214ff17509c9f8a19ae70f
kim-eunji <kim-eunji@kim-eunji.local>
2022-02-22 09:31:38 +0900

    Modify : SignUpView
    	- using bcrypt module hashed password

--- students/kimeunji/westagram/users/views.py
+++ students/kimeunji/westagram/users/views.py
@@ -1,5 +1,4 @@
-import json
-import re
+import json, re, bcrypt
 
 from django.http  import JsonResponse
 from django.views import View
@@ -23,12 +22,15 @@ class SignUpView(View):
             
             if User.objects.filter(email = email).exists():
                 return JsonResponse({'message':'EMAIL_ALREADY_EXISTS'}, status = 400)
+            
+            hashed_password         = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_hashed_password = hashed_password.decode('utf-8')
                 
             User.objects.create(
                 name         = data['name'],
                 email        = email,
                 phone_number = data['phone_number'],
-                password     = password
+                password     = decoded_hashed_password
             )
             return JsonResponse({'message':'SUCCESS'}, status = 201)
             
@@ -43,8 +45,8 @@ class LogInView(View):
             password = data['password']
             
             if not User.objects.filter(email = email).exists() or User.objects.get(email = email).password != password:
-                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
-            return JsonResponse({'message' : 'SUCCESS'}, status=200)
+                return JsonResponse({'message':'INVALID_USER'}, status = 401)
+            return JsonResponse({'message':'SUCCESS'}, status = 200)
         
         except KeyError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
