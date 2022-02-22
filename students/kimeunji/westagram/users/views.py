07c1d5ec987ec4cc9fe15c337d6153bf5d603c7a
kim-eunji <kim-eunji@kim-eunji.local>
2022-02-22 15:49:37 +0900

    Modify : LogInView 	- replaced filter method to get method 	- modified return messages 	- added ObjectDoesNotExist error solving code

--- students/kimeunji/westagram/users/views.py
+++ students/kimeunji/westagram/users/views.py
@@ -2,6 +2,7 @@ import json, re, bcrypt, jwt
 
 from django.http  import JsonResponse
 from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
 
 from users.models import User
 from westagram.settings import SECRET_KEY, ALGORITHM
@@ -44,18 +45,16 @@ class LogInView(View):
             data     = json.loads(request.body)
             email    = data['email']
             password = data['password']
-            user     = User.objects.filter(email=email)
-            
-            if not user.exists():
-                return JsonResponse({'message':'EMAIL_DOSE_NOT_EXISTS'}, status = 401)
-            
-            if bcrypt.checkpw(password.encode('utf-8'), user[0].password.encode('utf-8')):
-                token = jwt.encode({'user_id' : user[0].id}, SECRET_KEY, ALGORITHM)
+            user     = User.objects.get(email=email)
+                    
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                token = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
                 return JsonResponse({'message':'SUCCESS', 'token':token}, status = 200)
             
-            return JsonResponse({'message':'PASSWORD_INCORRECT'}, status = 401)
+            return JsonResponse({'message':'INVALID_USER'}, status = 401)
                 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status = 400)
-            
         
+        except ObjectDoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status = 401)
